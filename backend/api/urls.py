660aeca3eaae77925ee04f140fe61967e88904c2
aho <matsue4692@gmail.com>
2023-11-23 12:13:31 +0900

    Update: views.py urls.py viewを設定

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 
+from .views import CreateUserView, MyProfileView, TaskViewSet
+
 router = routers.DefaultRouter()
+router.register("tasks", TaskViewSet)
 
 urlpatterns = [
+    path("myself/", MyProfileView.as_view(), name="myself"),
+    path("register/", CreateUserView.as_view(), name="register"),
     path("", include(router.urls)),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
+
+from .models import Task
+from .serializers import TaskSerializer, UserSerializer
 
 # Create your views here.
+
+
+class CreateUserView(generics.CreateAPIView):
+    """
+    アカウント新規作成の時に使用する
+    """
+
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class MyProfileView(generics.RetrieveUpdateAPIView):
+    """
+    ログインしているユーザーのプロフィールを取得する
+    """
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    TaskをCLUDする
+    """
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
