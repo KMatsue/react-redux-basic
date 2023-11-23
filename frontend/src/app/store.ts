68f7d5a19bf6763d4155a6ff1505d738ef16d7d4
aho <matsue4692@gmail.com>
2023-11-23 14:43:29 +0900

    Add: loginSlice.ts ログイン用のSliceを実装

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 import counterReducer from "../features/basic/counter/counterSlice"
 import taskBasicReducer from "../features/basic/task_basic/taskBasicSlice"
 import fetchBasicReducer from "../features/basic/fetch_basic/fetchBasicSlice"
+import loginReducer from "../features/login/loginSlice"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     taskBasic: taskBasicReducer,
     fetchBasic: fetchBasicReducer,
+    login: loginReducer,
   },
 })
 
---
+++ frontend/src/features/login/loginSlice.ts
@@ -0,0 +1,104 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
+import { RootState } from "../../app/store"
+import axios from "axios"
+
+const apiUrl = "http://localhost:5173/"
+const token = localStorage.localJWT
+
+export interface loginState {
+  authen: { username: string; password: string }
+  isLoginView: boolean
+  profile: { id: number; username: string }
+  status: "idle" | "loading" | "failed"
+}
+
+export type TaskBasic = { id: number; title: string; completed: boolean }
+
+const initialState: loginState = {
+  authen: { username: "", password: "" },
+  isLoginView: true,
+  profile: { id: 0, username: "" },
+  status: "idle",
+}
+
+export const fetchAsyncLogin = createAsyncThunk(
+  "login/post",
+  async (auth: any) => {
+    const response = await axios.post(`${apiUrl}authen/jwt/create`, auth, {
+      headers: { "Content-Type": "application/json" },
+    })
+
+    return response.data
+  },
+)
+
+export const fetchAsyncRegister = createAsyncThunk(
+  "login/register",
+  async (auth: any) => {
+    const response = await axios.post(`${apiUrl}api/register`, auth, {
+      headers: { "Content-Type": "application/json" },
+    })
+
+    return response.data
+  },
+)
+
+export const fetchAsyncProf = createAsyncThunk(
+  "login/get",
+  async (auth: any) => {
+    const response = await axios.get(`${apiUrl}api/myself`, {
+      headers: { Authorization: `JWT ${token}` },
+    })
+
+    return response.data
+  },
+)
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    editUserName: (state, action: PayloadAction<string>) => {
+      state.authen.username = action.payload
+    },
+    editPassword: (state, action: PayloadAction<string>) => {
+      state.authen.password = action.payload
+    },
+    toggleMode: (state) => {
+      state.isLoginView = !state.isLoginView
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAsyncLogin.pending, (state) => {
+        state.status = "loading"
+      })
+      .addCase(fetchAsyncLogin.fulfilled, (state, action) => {
+        state.status = "idle"
+        localStorage.setItem("localJWT", action.payload.access)
+        action.payload.access && (window.location.href = "/tasks")
+      })
+      .addCase(fetchAsyncLogin.rejected, (state) => {
+        state.status = "failed"
+      })
+    builder
+      .addCase(fetchAsyncProf.pending, (state) => {
+        state.status = "loading"
+      })
+      .addCase(fetchAsyncProf.fulfilled, (state, action) => {
+        state.status = "idle"
+        state.profile = action.payload
+      })
+      .addCase(fetchAsyncProf.rejected, (state) => {
+        state.status = "failed"
+      })
+  },
+})
+
+export const { editUserName, editPassword, toggleMode } = loginSlice.actions
+
+export const selectAuthen = (state: RootState) => state.login.authen
+export const selectIsLoginView = (state: RootState) => state.login.isLoginView
+export const selectProfile = (state: RootState) => state.login.profile
+
+export default loginSlice.reducer
